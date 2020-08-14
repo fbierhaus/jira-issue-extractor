0fb5e2a1efdff434bd9c4ceea251355a7c1ad0cb
pashavictorovich <pavel@codefresh.io>
2020-08-15 00:47:25 +0300

    improve error messages

--- src/index.js
+++ src/index.js
@@ -28,20 +28,13 @@ async function execute() {
             });
         } catch (e) {
             if(!e.statusCode && JSON.parse(e).statusCode === 404) {
-                console.error(`Skip issue ${normalizedIssue}, didnt find in jira system`);
+                throw new Error(`Skip issue ${normalizedIssue}, didnt find in jira system`);
             } else {
-                try {
-                    const error = JSON.parse(e);
-                    if(error.statusCode === 401) {
-                        console.error('Wrong username or password');
-                        process.exit(1);
-                        return;
-                    }
-                    console.error(error.body);
-                } catch(err) {
-                    console.error(e.message);
+                const error = JSON.parse(e);
+                if(error.statusCode === 401) {
+                    throw new Error('Wrong username or password');
                 }
-                process.exit(1);
+                throw new Error(error.body);
             }
 
         }
