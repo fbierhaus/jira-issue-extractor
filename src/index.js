15689f0adbaa43be4159ddc90cc4ce9f365e8ed1
pashavictorovich <pavel@codefresh.io>
2020-08-15 00:56:41 +0300

    change to red color

--- src/index.js
+++ src/index.js
@@ -28,18 +28,18 @@ async function execute() {
             });
         } catch (e) {
             if(!e.statusCode && JSON.parse(e).statusCode === 404) {
-                console.log(`Skip issue ${normalizedIssue}, didnt find in jira system`, 'color:Red');
+                console.log(`Skip issue ${normalizedIssue}, didnt find in jira system`);
             } else {
                 try {
                     const error = JSON.parse(e);
                     if(error.statusCode === 401) {
-                        console.log('Wrong username or password', 'color:Red');
+                        console.error('Wrong username or password');
                         process.exit(1);
 
                     }
-                    console.log(error.body, 'color:Red');
+                    console.error(error.body);
                 } catch(err) {
-                    console.log(e.message, 'color:Red');
+                    console.error(e.message);
                 }
                 process.exit(1);
             }
